d2a37aacf23f3416baccacce0c8c33d60d990b6e
Murillo <mmbfreitas@hotmail.com>
2020-03-04 10:48:52 -0300

    test: add snapshots tests to components

--- src/components/atom/input/Input.test.js
+++ src/components/atom/input/Input.test.js
@@ -1,1 +1,12 @@
-"use strict";
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Input from './Input';
+
+describe('Input', () => {
+  it('should match with the snapshot', () => {
+    const wrapper = renderer
+      .create(<Input placeholder={'placeholder text'} id={'ipt'} name={'ipt'} type={'text'}></Input>)
+      .toJSON();
+    expect(wrapper).toMatchSnapshot();
+  });
+});
