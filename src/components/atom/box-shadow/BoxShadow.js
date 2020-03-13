89a2b7aea2edac9e950108d90183b597665f4930
Kaio <kaiolf11@gmail.com>
2020-03-13 14:29:13 -0300

    feature: add radius prop to box-shadow component

--- src/components/atom/box-shadow/BoxShadow.js
+++ src/components/atom/box-shadow/BoxShadow.js
@@ -4,13 +4,14 @@ import PropTypes from 'prop-types';
 
 import css from './BoxShadow.module.scss';
 
-const BoxShadow = ({ level, children }) => {
+const BoxShadow = ({ level, radius, children }) => {
   const classes = classNames({
     [css['elevation--1']]: level === 1,
     [css['elevation--2']]: level === 2,
     [css['elevation--3']]: level === 3,
     [css['elevation--4']]: level === 4,
-    [css['elevation--5']]: level === 5
+    [css['elevation--5']]: level === 5,
+    [css[`c-box-shadow--radius-${radius}`]]: true
   });
 
   return <div className={classes}>{children}</div>;
@@ -18,11 +19,13 @@ const BoxShadow = ({ level, children }) => {
 
 BoxShadow.propTypes = {
   level: PropTypes.oneOf([1, 2, 3, 4, 5]).isRequired,
+  radius: PropTypes.oneOf([1, 2, 3, 4]),
   children: PropTypes.oneOfType([PropTypes.array, PropTypes.object])
 };
 
 BoxShadow.defaultProps = {
-  children: null
+  children: null,
+  radius: 2
 };
 
 export default BoxShadow;
