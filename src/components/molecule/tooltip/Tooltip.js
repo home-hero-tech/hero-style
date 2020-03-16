3f401cd3e6b96abd0c6c87c0c73a690e8e788486
Matheus Alexandre <mathe.system@gmail.com>
2020-03-16 15:22:50 -0300

    chore: Added Tooltip component

---
+++ src/components/molecule/tooltip/Tooltip.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import css from './Tooltip.module.scss';
+
+const { Tooltip: Wrapper } = require('react-tippy');
+
+const ToolTip = ({ description, animation, children, ...otherProps }) => {
+  return (
+    <Wrapper
+      arrow
+      animation={animation}
+      className={css['c-tooltip']}
+      title={description}
+      {...otherProps}
+    >
+      {children}
+    </Wrapper>
+  );
+};
+
+ToolTip.propTypes = {
+  animation: PropTypes.oneOf(['shift', 'perspective', 'fade', 'scale', 'none']),
+  children: PropTypes.oneOfType([
+    PropTypes.array,
+    PropTypes.object,
+    PropTypes.string
+  ]).isRequired,
+  description: PropTypes.string.isRequired
+};
+
+ToolTip.defaultProps = {
+  animation: 'perspective'
+};
+
+export default ToolTip;
