3a337d0fdb5f04b961d62b775c26654a3b4bb915
Matheus Alexandre <mathe.system@gmail.com>
2020-03-20 14:39:38 -0300

    release: v1.0.63

--- src/docs/stories/11-Heading.stories.js
+++ src/docs/stories/11-Heading.stories.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Heading from '../../components/atom/heading/Heading';
+import Wrapper from '../../helpers/wrapper/Wrapper';
 
 export default {
   title: 'Heading',
@@ -7,16 +8,46 @@ export default {
 };
 
 const someStyle = {
-  color: 'blue'
+  color: 'blue',
+  paddingLeft: 10
 };
 
-export const H1 = () => <Heading level={1} title="Title" />;
-export const H2 = () => <Heading level={2} title="Title" />;
-export const H3 = () => <Heading level={3} title="Title" />;
-export const H4 = () => <Heading level={4} title="Title" />;
-export const H5 = () => <Heading level={5} title="Title" />;
-export const H6 = () => <Heading level={6} title="Title" />;
+export const level = () => (
+  <Wrapper>
+    <Heading level={1} title="h1 - Title" />
+    <Heading level={2} title="h2 - Title" />
+    <Heading level={3} title="h3 - Title" />
+    <Heading level={4} title="h4 - Title" />
+    <Heading level={5} title="h5 - Title" />
+    <Heading level={6} title="h6 - Title" />
+  </Wrapper>
+);
+
 export const withMargin = () => <Heading level={1} title="Title" withMargin />;
+
+export const weights = () => (
+  <Wrapper>
+    <Heading level={1} title="Title" bold />
+    <Heading level={1} title="Title" />
+    <Heading level={1} title="Title" light />
+  </Wrapper>
+);
+
+export const transforms = () => (
+  <Wrapper>
+    <Heading level={3} title="Title" uppercase />
+    <Heading level={3} title="Title" lowercase />
+    <Heading level={3} title="Title" capitalize />
+  </Wrapper>
+);
+
+export const colors = () => (
+  <Wrapper>
+    <Heading level={1} title="Title" />
+    <Heading level={1} title="Title" gray />
+  </Wrapper>
+);
+
 export const withOtherProps = () => (
   <Heading level={6} title="Title" style={someStyle} />
 );
