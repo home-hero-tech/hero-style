516f0aac9a88071184a86b11601eff2799b534c6
Kaio Ferreira <kaiolf11@gmail.com>
2020-03-20 09:51:24 -0300

    feature: add masked input component (#10)

---
+++ src/components/atom/masked-input/masks.js
@@ -0,0 +1,57 @@
+export const cnpjMask = [
+  /\d/,
+  /\d/,
+  '.',
+  /\d/,
+  /\d/,
+  /\d/,
+  '.',
+  /\d/,
+  /\d/,
+  /\d/,
+  '/',
+  /\d/,
+  /\d/,
+  /\d/,
+  /\d/,
+  '-',
+  /\d/,
+  /\d/
+];
+
+export const cpfMask = [
+  /\d/,
+  /\d/,
+  /\d/,
+  '.',
+  /\d/,
+  /\d/,
+  /\d/,
+  '.',
+  /\d/,
+  /\d/,
+  /\d/,
+  '-',
+  /\d/,
+  /\d/
+];
+
+export const phoneMask = [
+  '(',
+  /\d/,
+  /\d/,
+  ')',
+  ' ',
+  /\d/,
+  /\d/,
+  /\d/,
+  /\d/,
+  /\d/,
+  '-',
+  /\d/,
+  /\d/,
+  /\d/,
+  /\d/
+];
+
+export default { cnpjMask, cpfMask, phoneMask };
