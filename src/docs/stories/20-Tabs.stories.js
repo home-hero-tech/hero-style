71f4e74ad29d8d442d223122965d7f483a589d69
Matheus Alexandre <mathe.system@gmail.com>
2020-03-26 17:17:35 -0300

    Feature/tabs (#12)
    * fix: Fixing eslint
    
    * chore: Created two new components, one is a normal React-Tab component and the other is Scrollable Tab component
    
    * chore: Created two new components, one is a normal React-Tab component and the other is Scrollable Tab component

---
+++ src/docs/stories/20-Tabs.stories.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+// react-tabs
+import { TabList, TabPanel, Tab } from 'react-tabs';
+// rc-tabs
+import { TabPane as ScrollablePanel } from 'rc-tabs';
+
+import Tabs from '../../components/molecule/tabs/Tabs';
+import ScrollableTabs from '../../components/molecule/tabs/ScrollableTabs';
+
+import Wrapper from '../../helpers/wrapper/Wrapper';
+
+export default {
+  title: 'Tabs',
+  component: Tabs
+};
+
+const style = {
+  width: '100%',
+  height: '100%'
+};
+
+export const Default = () => (
+  <Wrapper style={style}>
+    <Tabs>
+      <TabList>
+        <Tab>First tab</Tab>
+        <Tab>Second tab</Tab>
+        <Tab>Third tab</Tab>
+      </TabList>
+      <TabPanel>First content</TabPanel>
+      <TabPanel>Second content</TabPanel>
+      <TabPanel>Third content</TabPanel>
+    </Tabs>
+  </Wrapper>
+);
+
+export const Scrollable = () => {
+  const [activeKey, setActiveKey] = useState('1');
+
+  const onChange = currentKey => setActiveKey(currentKey);
+
+  const onTabClick = key => {
+    if (key !== activeKey) {
+      onChange('');
+    }
+  };
+
+  const numbers = {
+    1: 'First',
+    2: 'Second',
+    3: 'Third',
+    4: 'Fourth'
+  };
+
+  return (
+    <Wrapper style={style}>
+      <ScrollableTabs
+        onChange={onChange}
+        onTabClick={onTabClick}
+        activeKey={activeKey}
+      >
+        {[1, 2, 3, 4].map((item, index) => (
+          <ScrollablePanel
+            tab={`${numbers[index + 1]} tab`}
+            key={`${index + 1}`}
+          >
+            {index + 1} - Lorem ipsum dolor sit amet, consectetur adipisicing
+            elit. Animi commodi cumque fuga repellendus voluptas. Delectus
+            distinctio doloribus error eveniet fugiat neque odit quas recusandae
+            totam? Deserunt distinctio est iste reiciendis.
+          </ScrollablePanel>
+        ))}
+        <ScrollablePanel tab="Disabed" key="5" disabled />
+      </ScrollableTabs>
+    </Wrapper>
+  );
+};
