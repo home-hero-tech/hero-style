eff2e4ecd191926dd03adf3b4a5554863e03d7c1
Kaio Ferreira <kaiolf11@gmail.com>
2020-03-29 21:29:35 -0300

    feat: add collapse component

---
+++ src/components/atom/collapse/Collapse.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import css from './Collapse.module.scss';
+
+const Collapse = ({ isOpen, children }) => {
+  return (
+    <div className={css[`c-collapse${isOpen ? '--open' : ''}`]}>
+      <div className={css[`c-collapse__content`]}>{children}</div>
+    </div>
+  );
+};
+
+Collapse.propTypes = {
+  isOpen: PropTypes.bool,
+  children: PropTypes.instanceOf(PropTypes.object).isRequired
+};
+
+Collapse.defaultProps = {
+  isOpen: false
+};
+
+export default Collapse;
