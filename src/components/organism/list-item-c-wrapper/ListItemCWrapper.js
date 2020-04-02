33cf7eaafeddc6d802ebf1b207d0cc9501a5a61a
Matheus Alexandre <mathe.system@gmail.com>
2020-04-01 22:00:05 -0300

    chore: Created CardLine component and some List components to help in… (#17)
    * chore: Created CardLine component and some List components to help in its use
    
    * fix: Removed useless code

---
+++ src/components/organism/list-item-c-wrapper/ListItemCWrapper.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import css from './ListItemCWrapper.module.scss';
+
+const ListItemCWrapper = ({ children }) => {
+  return <div className={css['c-wrapper']}>{children}</div>;
+};
+
+ListItemCWrapper.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.object, PropTypes.array]).isRequired
+};
+ListItemCWrapper.defaultProps = {};
+ListItemCWrapper.displayName = 'ListItemCWrapper';
+
+export default ListItemCWrapper;
