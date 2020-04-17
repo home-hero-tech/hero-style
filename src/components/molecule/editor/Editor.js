ed7ba5af94e017c94c9a89080121b67f5382c530
Kaio Ferreira <kaiolf11@gmail.com>
2020-04-17 14:08:22 -0300

    fix: adjust css imports

--- src/components/molecule/editor/Editor.js
+++ src/components/molecule/editor/Editor.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactQuill from 'react-quill';
-
-import 'react-quill/dist/quill.snow.css';
 import './Editor.scss';
 
 const Editor = ({ value, onChange, toolbar, ...otherProps }) => {
