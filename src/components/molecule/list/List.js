15eeb7c37553189b9bab874d504908be4f5e9ec0
Murillo <mmbfreitas@hotmail.com>
2020-05-08 15:56:02 -0300

    fix: allow style in list

--- src/components/molecule/list/List.js
+++ src/components/molecule/list/List.js
@@ -5,23 +5,29 @@ import PropTypes from 'prop-types';
 
 import css from './List.module.scss';
 
-const List = ({ level, children }) => {
+const List = ({ level, children, style }) => {
   const classes = classNames({
     [css['c-list']]: true,
     [css[`c-list--${level}`]]: true
   });
 
-  return <ul className={classes}>{children}</ul>;
+  return (
+    <ul className={classes} style={style}>
+      {children}
+    </ul>
+  );
 };
 
 List.propTypes = {
   children: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
-  level: PropTypes.number
+  level: PropTypes.number,
+  style: PropTypes.oneOfType([PropTypes.object, PropTypes.array])
 };
 
 List.defaultProps = {
   children: null,
-  level: 1
+  level: 1,
+  style: null
 };
 
 export default List;
