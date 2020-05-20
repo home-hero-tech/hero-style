0e5ab92f06234263e983d6d296a826cce65aee08
Kaio Ferreira <kaiolf11@gmail.com>
2020-05-20 15:23:03 -0300

    chore: add props to ModalContent

--- src/components/molecule/modal-content/ModalContent.js
+++ src/components/molecule/modal-content/ModalContent.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
 import css from '../../organism/modal/Modal.module.scss';
 
-const ModalContent = ({ children }) => (
-  <main className={css['c-modal__content']}>{children}</main>
-);
+const ModalContent = ({ className, children }) => {
+  const classes = classNames(css['c-modal__content'], className);
+  return <main className={classes}>{children}</main>;
+};
 
 ModalContent.propTypes = {
+  className: PropTypes.string,
   children: PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired
+};
+
+ModalContent.defaultProps = {
+  className: ''
 };
 
 ModalContent.defaultProps = {};
