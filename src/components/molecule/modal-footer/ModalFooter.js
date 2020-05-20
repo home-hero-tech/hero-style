93f404b755fb6e1ad65f444817fcdde72ff538ac
Kaio Ferreira <kaiolf11@gmail.com>
2020-05-20 11:59:09 -0300

    Release v1.0.212 (#47)
    * Feature/card multi select (#43)
    
    * feat: add prop multiple to CardSelect component
    
    * feat: default autocomplete prop to off in Form component
    
    * chore: fix Editor component border radius
    
    * release: v1.0.203
    
    * feat: add alignment prop to modal footer (#46)
    
    * release: v1.0.212
    
    Co-authored-by: Murillo <mmbfreitas@hotmail.com>

--- src/components/molecule/modal-footer/ModalFooter.js
+++ src/components/molecule/modal-footer/ModalFooter.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 import css from '../../organism/modal/Modal.module.scss';
 
-const ModalFooter = ({ children }) => (
-  <footer className={css['c-modal__actions']}>{children}</footer>
-);
+const ModalFooter = ({ className, children, align }) => {
+  const classes = classNames(css['c-modal__actions'], className, {
+    'c-modal__actions--align-end': align === 'end',
+    'c-modal__actions--align-between': align === 'between'
+  });
+  return <footer className={classes}>{children}</footer>;
+};
 
 ModalFooter.propTypes = {
+  className: PropTypes.string,
+  align: PropTypes.oneOf(['between', 'end']),
   children: PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired
 };
 
-ModalFooter.defaultProps = {};
+ModalFooter.defaultProps = {
+  className: '',
+  align: 'between'
+};
 
 ModalFooter.displayName = 'ModalFooter';
 
