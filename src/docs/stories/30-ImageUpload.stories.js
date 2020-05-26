3bce2616c622d0a69e1892abbf6b42d8784e3de0
Kaio Ferreira <kaiolf11@gmail.com>
2020-05-26 17:29:56 -0300

    feat: add upload image component

---
+++ src/docs/stories/30-ImageUpload.stories.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import ImageUpload from '../../components/atom/image-upload/ImageUpload';
+
+export default {
+  title: 'ImageUpload',
+  component: ImageUpload
+};
+
+export const Default = () => {
+  const [value, setValue] = useState(null);
+
+  const handleChange = e => setValue(e.target.files[0]);
+
+  return (
+    <div>
+      <ImageUpload
+        id="image-upload"
+        name="ipt"
+        text="Buscar arquivo"
+        value={value}
+        onChange={handleChange}
+        onRemove={() => setValue(null)}
+      />
+    </div>
+  );
+};
+
+export const Disabled = () => {
+  const [value, setValue] = useState(null);
+  return (
+    <div>
+      <ImageUpload
+        id="image-upload"
+        name="ipt"
+        text="Buscar arquivo"
+        value={value}
+        onChange={setValue}
+        disabled
+      />
+    </div>
+  );
+};
