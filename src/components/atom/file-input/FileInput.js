eee3f0278b0cd6d5fcd84ea8624549d964a40066
Kaio Ferreira <kaiolf11@gmail.com>
2020-05-28 16:13:04 -0300

    Release/v1.5.0 (#54)
    * Feature/new component props (#53)
    
    * feat: add href prop to card item
    
    * feat: add valSuccess prop to CardItem
    
    * chore: set button display to flex when there's an svg
    
    * feat: add rows prop to editor component
    
    * Feature/upload component (#52)
    
    * feat: add upload image component
    
    * chore: export image upload component
    
    * chore: disable file selection when button is disabled
    
    * chore: bump version to 1.5.0

--- src/components/atom/file-input/FileInput.js
+++ src/components/atom/file-input/FileInput.js
@@ -2,11 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCloudUpload } from '@fortawesome/pro-light-svg-icons';
+import classNames from 'classnames';
+
 import css from './FileInput.module.scss';
 
-const FileInput = ({ id, name, text, mult, ...props }) => {
+const FileInput = ({ id, name, text, mult, className, ...props }) => {
+  const classes = classNames(className, css['c-file']);
   return (
-    <div className={css['c-file']}>
+    <div className={classes}>
       {mult ? (
         <input id={id} name={name} type="file" {...props} multiple />
       ) : (
@@ -24,13 +27,15 @@ FileInput.propTypes = {
   id: PropTypes.string,
   name: PropTypes.string,
   text: PropTypes.string,
-  mult: PropTypes.bool
+  mult: PropTypes.bool,
+  className: PropTypes.string
 };
 
 FileInput.defaultProps = {
   id: null,
   name: null,
   text: '',
+  className: '',
   mult: false
 };
 
