c8518c782eeaff3e9f7ec039d1723a9a33ecd976
Kaio Ferreira <kaiolf11@gmail.com>
2020-08-13 14:23:46 -0300

    feat: add slider component

---
+++ src/components/molecule/slider/Slider.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import SlickSlider from 'react-slick';
+
+import css from './Slider.module.scss';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const Slider = ({ children, settings, ...otherProps }) => {
+  const renderItem = (item, index) => (
+    <div key={index} className={css['c-slider__item']}>
+      {item}
+    </div>
+  );
+
+  return (
+    <div className={css['c-slider']} {...otherProps}>
+      <SlickSlider {...settings}>{children.map(renderItem)}</SlickSlider>
+    </div>
+  );
+};
+
+Slider.propTypes = {
+  settings: PropTypes.shape({
+    dots: PropTypes.bool,
+    infinite: PropTypes.bool,
+    speed: PropTypes.number,
+    slidesToShow: PropTypes.number,
+    slidesToScroll: PropTypes.number
+  }),
+  children: PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired
+};
+
+Slider.defaultProps = {
+  settings: {
+    // dots: false,
+    // arrows: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  }
+};
+
+export default Slider;
---
+++ src/docs/stories/32-Slider.stories.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Slider from '../../components/molecule/slider/Slider';
+import Card from '../../components/molecule/card/Card';
+import CardHeader from '../../components/atom/card-header/CardHeader';
+import CardBody from '../../components/atom/card-body/CardBody';
+
+export default {
+  title: 'Slider',
+  component: Slider
+};
+
+const renderCard = item => (
+  <Card>
+    <CardHeader>Header {item}</CardHeader>
+    <CardBody>Conteúdo do card numero {item}</CardBody>
+  </Card>
+);
+
+export const Default = () => {
+  return (
+    <Slider style={{ maxWidth: 800, margin: '0 auto' }}>
+      {[1, 2, 3, 4, 5, 6].map(renderCard)}
+    </Slider>
+  );
+};
