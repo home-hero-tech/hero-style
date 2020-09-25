d5cec5ba7a4b40e2e7d2e7e5ca9b36bebb9e3fde
Matheus Alexandre <mathe.system@gmail.com>
2020-09-25 16:27:56 -0300

    release: v1.21.11

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -747,4 +747,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [1.21.10] - 2020-09-25
 ### Add
+- Repassing other props to DropdownMenu component.
+
+## [1.21.11] - 2020-09-25
+### Add
 - Repassing other props to Dropdown component.
--- src/components/molecule/dropdown/Dropdown.js
+++ src/components/molecule/dropdown/Dropdown.js
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types';
 
 import css from './Dropdown.module.scss';
 
-const Dropdown = ({ children }) => {
-  return <div className={css['c-dropdown']}>{children}</div>;
-};
+const Dropdown = ({ children, ...otherProps }) => (
+  <div className={css['c-dropdown']} {...otherProps}>
+    {children}
+  </div>
+);
 
 Dropdown.propTypes = {
   children: PropTypes.oneOfType([PropTypes.string, PropTypes.object, PropTypes.array]).isRequired
