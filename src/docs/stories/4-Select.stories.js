6f0d5d21373828541a5aede1d59ff3582066e337
Matheus Alexandre <mathe.system@gmail.com>
2020-10-13 12:52:35 -0300

    feat: several improvements on Select component

--- src/docs/stories/4-Select.stories.js
+++ src/docs/stories/4-Select.stories.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLink } from '@fortawesome/pro-light-svg-icons';
 import Select from '../../components/atom/select/Select';
+import Wrapper from '../../helpers/wrapper/Wrapper';
+import Spacer from '../../components/molecule/spacer/Spacer';
 
 export default {
   title: 'Select',
@@ -18,78 +20,89 @@ const options = [
 const placeholder = 'Selecione...';
 const noOptionsMessage = () => 'Nenhuma opção';
 
-export const Default = () => <Select options={options} placeholder={placeholder} value={options[3]} isClearable />;
+export const Default = () => (
+  <Wrapper>
+    <Select options={options} placeholder={placeholder} value={options[3]} isClearable />
+  </Wrapper>
+);
 
 export const withIcon = () => (
-  <Select options={options} placeholder={placeholder} value={options[3]} icon={<FontAwesomeIcon icon={faLink} />} />
-);
-
-export const Dark = () => (
-  <Select options={options} placeholder={placeholder} noOptionsMessage={noOptionsMessage} value={options[3]} dark />
-);
-
-export const small = () => (
-  <Select options={options} placeholder={placeholder} noOptionsMessage={noOptionsMessage} small />
-);
-
-export const smallWithIcon = () => (
-  <Select
-    options={options}
-    placeholder={placeholder}
-    noOptionsMessage={noOptionsMessage}
-    small
-    icon={<FontAwesomeIcon icon={faLink} />}
-  />
-);
-
-export const SmallDark = () => (
-  <Select options={options} placeholder={placeholder} noOptionsMessage={noOptionsMessage} dark small isClearable />
+  <Wrapper>
+    <Select options={options} placeholder={placeholder} value={options[3]} icon={<FontAwesomeIcon icon={faLink} />} />
+  </Wrapper>
 );
 
 export const MultiSelect = () => (
-  <Select options={options} placeholder={placeholder} noOptionsMessage={noOptionsMessage} multiple />
-);
-export const SearchableOff = () => (
-  <Select
-    options={options}
-    placeholder="Digite para encontrar uma opção"
-    noOptionsMessage={noOptionsMessage}
-    searchable={false}
-  />
+  <Wrapper>
+    <Select options={options} placeholder={placeholder} noOptionsMessage={noOptionsMessage} multiple />
+  </Wrapper>
 );
 
-export const ColorApproved = () => (
-  <Select approved options={options} placeholder={placeholder} value={options[3]} isClearable />
+export const SearchableOff = () => (
+  <Wrapper>
+    <Select
+      options={options}
+      placeholder="Digite para encontrar uma opção"
+      noOptionsMessage={noOptionsMessage}
+      searchable={false}
+    />
+  </Wrapper>
 );
-export const SmallApproved = () => (
-  <Select small approved options={options} placeholder={placeholder} value={options[3]} isClearable />
-);
-export const ColorPending = () => (
-  <Select options={options} placeholder={placeholder} value={options[3]} isClearable pending />
-);
-export const SmallPending = () => (
-  <Select options={options} placeholder={placeholder} value={options[3]} isClearable pending small />
-);
-export const ColorRejected = () => (
-  <Select rejected options={options} placeholder={placeholder} value={options[3]} isClearable />
-);
-export const RejectedWithIcon = () => (
-  <Select
-    icon={<FontAwesomeIcon icon={faLink} />}
-    rejected
-    options={options}
-    placeholder={placeholder}
-    value={options[3]}
-    isClearable
-  />
-);
+
 export const Disabled = () => (
-  <Select
-    isDisabled
-    options={options}
-    value={options[3]}
-    placeholder="Digite para encontrar uma opção"
-    noOptionsMessage={noOptionsMessage}
-    searchable={false}
-  />
+  <Wrapper>
+    <Select
+      isDisabled
+      options={options}
+      value={options[3]}
+      placeholder="Digite para encontrar uma opção"
+      noOptionsMessage={noOptionsMessage}
+      searchable={false}
+    />
+  </Wrapper>
+);
+
+export const types = () => (
+  <Wrapper>
+    <Select options={options} placeholder={placeholder} noOptionsMessage={noOptionsMessage} />
+    <Spacer myLg={1} myMd={1} mySm={1} myXs={1}>
+      <Select
+        options={options}
+        placeholder={placeholder}
+        noOptionsMessage={noOptionsMessage}
+        icon={<FontAwesomeIcon icon={faLink} />}
+      />
+    </Spacer>
+    <Select options={options} placeholder="Approved" approved />
+    <Spacer myLg={1} myMd={1} mySm={1} myXs={1}>
+      <Select options={options} placeholder="Rejected" rejected />
+    </Spacer>
+    <Select options={options} placeholder="Pending" pending isClearable />
+    <Spacer myLg={1} myMd={1} mySm={1} myXs={1}>
+      <Select options={options} placeholder="Dark" noOptionsMessage={noOptionsMessage} dark isClearable />
+    </Spacer>
+  </Wrapper>
+);
+
+export const smallTypes = () => (
+  <Wrapper>
+    <Select options={options} placeholder={placeholder} noOptionsMessage={noOptionsMessage} small />
+    <Spacer myLg={1} myMd={1} mySm={1} myXs={1}>
+      <Select
+        options={options}
+        placeholder={placeholder}
+        noOptionsMessage={noOptionsMessage}
+        small
+        icon={<FontAwesomeIcon icon={faLink} />}
+      />
+    </Spacer>
+    <Select options={options} placeholder="Approved" small approved />
+    <Spacer myLg={1} myMd={1} mySm={1} myXs={1}>
+      <Select options={options} placeholder="Rejected" small rejected />
+    </Spacer>
+    <Select options={options} placeholder="Pending" small pending isClearable />
+    <Spacer myLg={1} myMd={1} mySm={1} myXs={1}>
+      <Select options={options} placeholder="Dark" noOptionsMessage={noOptionsMessage} dark small isClearable />
+    </Spacer>
+  </Wrapper>
 );
