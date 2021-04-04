872999e1d9dc52f7096809db50766fe5641c7b7b
Murillo <mmbfreitas@hotmail.com>
2021-04-04 17:31:29 -0300

    refactor: add dropdown to filter header

--- src/components/molecule/filter-header/FilterHeader.js
+++ src/components/molecule/filter-header/FilterHeader.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFilter, faSort } from '@fortawesome/pro-light-svg-icons';
+import { faFilter } from '@fortawesome/pro-light-svg-icons';
 import BoxShadow from '../../atom/box-shadow/BoxShadow';
 import Text from '../../atom/text/Text';
 
 import css from '../../organism/filter/Filter.module.scss';
 
-const FilterHeader = ({ onClick, tabIndex, totalItems, handleOrder, children }) => (
+const FilterHeader = ({ onClick, tabIndex, totalItems, dropDown, children }) => (
   <BoxShadow level={1} radius={4}>
     <div className={css['c-filter__header']} onClick={onClick} role="button" tabIndex={tabIndex} onKeyPress={() => {}}>
       <div className={css['c-filter__heading']}>
@@ -22,11 +22,7 @@ const FilterHeader = ({ onClick, tabIndex, totalItems, handleOrder, children }) 
         </Text>
       </div>
       {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/interactive-supports-focus */}
-      <div onClick={handleOrder} role="button" className={css['c-filter__heading']}>
-        <Text end regular>
-          <FontAwesomeIcon icon={faSort} /> Ordenar
-        </Text>
-      </div>
+      {dropDown || null}
     </div>
     {children}
   </BoxShadow>
@@ -34,7 +30,7 @@ const FilterHeader = ({ onClick, tabIndex, totalItems, handleOrder, children }) 
 
 FilterHeader.propTypes = {
   onClick: PropTypes.func,
-  handleOrder: PropTypes.func,
+  dropDown: PropTypes.instanceOf(Object),
   tabIndex: PropTypes.number,
   totalItems: PropTypes.number,
   children: PropTypes.oneOfType([PropTypes.object])
@@ -43,7 +39,6 @@ FilterHeader.propTypes = {
 FilterHeader.defaultProps = {
   children: null,
   onClick: null,
-  handleOrder: f => f,
   tabIndex: 0,
   totalItems: 0
 };
